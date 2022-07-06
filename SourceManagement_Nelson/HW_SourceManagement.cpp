0470dee57671eaf29e2270b42250eb5282674426
Molly <NekoNyani@outlook.com>
2022-07-06 14:13:58 -0400

    First Comment

---
+++ SourceManagement_Nelson/HW_SourceManagement.cpp
@@ -0,0 +1,17 @@
+//Molly Nelson
+//07-6-22
+//CIS1202
+//Understanding source management on "Hello World" code
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello World :) \n\n";
+
+	system("pause");
+	return 0;
+}
